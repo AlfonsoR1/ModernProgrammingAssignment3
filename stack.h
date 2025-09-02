59dca4f1d2112ee8bf9b2857796ec93cd21d4049
Alfonso R <alfonsorocha101@gmail.com>
2025-09-02 00:05:42 -0500

    added stack.h to the end of the comment on line 5 for better identification

--- stack.h
+++ stack.h
@@ -2,7 +2,7 @@
 *  Name: Alfonso Rocha
 *  Assignment: Assignment 3: Best Practices
 *  Purpose: A demonstration of a simple stack ADT and finding
-*           all violations of best practices.
+*           all violations of best practices on stack.h
 ************************************************* */
 
 #ifndef STACK_H
