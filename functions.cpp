976b0463a2bef7743a890220454af6693ddaaaca
Alfonso R <alfonsorocha101@gmail.com>
2025-09-03 22:20:44 -0500

    Added functions.h and functions.cpp files. with file headers and updated .gitignore to track files

---
+++ functions.cpp
@@ -0,0 +1,7 @@
+/* *************************************************
+*  Name: Alfonso Rocha
+*  Assignment: Assignment 3: Best Practices
+*  Purpose: A demonstration of a simple stack ADT and finding
+*           all violations of best practices on
+*           definition file, print_functions.cpp
+************************************************* */
---
+++ functions.h
@@ -0,0 +1,12 @@
+/* *************************************************
+*  Name: Alfonso Rocha
+*  Assignment: Assignment 3: Best Practices
+*  Purpose: A demonstration of a simple stack ADT and finding
+*           all violations of best practices on
+*           declaration file, functions.h
+**************************************************/
+
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#endif //FUNCTIONS_H
