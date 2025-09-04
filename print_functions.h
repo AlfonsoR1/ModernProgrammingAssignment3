087c5e943b899e521165f742ef9156e1ec37587c
Alfonso R <alfonsorocha101@gmail.com>
2025-09-03 20:15:44 -0500

    initial commit of print_functions.h and print_functions.cpp, Added files to .gitignore and Added print opening and clsoing to print_functions.h

---
+++ print_functions.h
@@ -0,0 +1,16 @@
+//
+// Created by alfonso on 9/3/25.
+//
+
+#ifndef PRINT_FUNCTIONS_H
+#define PRINT_FUNCTIONS_H
+
+
+void printopening() {
+    std::cout << "Beginning tests..." << std::endl;
+}
+
+void printclosing() {std::cout << "Testing complete." << std::endl;}
+
+
+#endif //PRINT_FUNCTIONS_H
