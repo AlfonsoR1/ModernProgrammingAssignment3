1c934711d9a0ea63476125049e031ed883a5ddd1
Alfonso R <alfonsorocha101@gmail.com>
2025-09-04 17:54:48 -0500

    use testCounters struct in main.cpp instead of individual counters. Added underFlowTests() call to main.cpp. commented out main.cpp code for underFlowTests testing. Testing was successful

--- main.cpp
+++ main.cpp
@@ -24,72 +24,15 @@ int main() {
     int value;
 
     // counters for tests
-    int is_empty = 0;
-    int peeked = 0;
-    int popped = 0;
-    int pushed = 0;
+    testCounters counters{};
 
     printopening();
+    underFlowTests(stack,counters,value);
 
-
-    /************************************************
-    ******* UNDERFLOW TESTS *************************
-    ************************************************/
-
-    std::cout << "  underflow tests ==> ";
-
-    for (int i = 0; i < STACKSIZE*MULTIPLIER; i++) {
-
-        if (stack.isEmpty()) is_empty++;
-        else is_empty--;
-
-        if(stack.peek(&value)) peeked++;
-        else peeked--;
-    
-        try{
-            value = stack.pop();
-            popped++;
-        } catch(...){
-            // should always decrement
-            popped--;
-        }
-
-        if (stack.push(i)){
-            // should always increment
-            pushed++;
-            if(stack.peek(&value)){
-                // should always increment
-                peeked++;
-            } else {
-                peeked--;
-            }
-            try{ // returns to empty
-                // should always increment
-                value = stack.pop();
-                popped++;
-            } catch(...){
-                popped--;
-            }
-        } else{
-            pushed--;
-        }
-    }
-    
-    // notice these numbers are logical
-    // in underflow, we should expect these numbers
-    if(popped == 0 &&
-       peeked == 0 &&
-       pushed == round(STACKSIZE*MULTIPLIER) && 
-       is_empty == round(STACKSIZE*MULTIPLIER)) {
-       std::cout << "pass" << std::endl;
-    } else {
-        std::cout << "failed" << std::endl;
-    }
-    
     /************************************************
     ******* OVERFLOW TESTS **************************
     ************************************************/
-
+/*
     std::cout << "  overflow tests ==> ";
 
     // reset counters for tests
@@ -161,7 +104,7 @@ int main() {
 
     /************************************************
     ******** EMPTYING AND MORE UNDERFLOW TESTS ******
-    ************************************************/
+    ************************************************
     // reset counters for tests
     is_empty = 0;
     peeked = 0;
@@ -201,7 +144,7 @@ int main() {
 
     /************************************************
     ******** MID-STACK TESTS ************************
-    ************************************************/
+    ************************************************
     // reset counters for tests
     is_empty = 0;
     peeked = 0;
