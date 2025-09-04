9da0e3aae732f05edb52e82385cc88a158dca93a
Alfonso R <alfonsorocha101@gmail.com>
2025-09-03 20:20:23 -0500

    Added detailed file headers to print_functions.h and print_functions.cpp

--- print_functions.cpp
+++ print_functions.cpp
@@ -1,3 +1,7 @@
-//
-// Created by alfonso on 9/3/25.
-//
+/* *************************************************
+*  Name: Alfonso Rocha
+*  Assignment: Assignment 3: Best Practices
+*  Purpose: A demonstration of a simple stack ADT and finding
+*           all violations of best practices on
+*           definition file, print_functions.cpp
+************************************************* */
--- print_functions.h
+++ print_functions.h
@@ -1,6 +1,10 @@
-//
-// Created by alfonso on 9/3/25.
-//
+/* *************************************************
+*  Name: Alfonso Rocha
+*  Assignment: Assignment 3: Best Practices
+*  Purpose: A demonstration of a simple stack ADT and finding
+*           all violations of best practices on
+*           declaration file, print_functions.h
+**************************************************/
 
 #ifndef PRINT_FUNCTIONS_H
 #define PRINT_FUNCTIONS_H
