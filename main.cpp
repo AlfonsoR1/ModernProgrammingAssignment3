4112981d5b0b535084c361b07b0e3ba8ee01d946
Alfonso R <alfonsorocha101@gmail.com>
2025-09-03 21:43:12 -0500

    Fixed bug by removing the extra print closing() on line 33

--- main.cpp
+++ main.cpp
@@ -30,7 +30,7 @@ int main() {
     int pushed = 0;
 
     printopening();
-    printclosing();
+
 
     /************************************************
     ******* UNDERFLOW TESTS *************************
