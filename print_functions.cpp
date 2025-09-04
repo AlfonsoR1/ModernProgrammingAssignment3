15ae9ab3b280d17543002b3f732933535706351f
Alfonso R <alfonsorocha101@gmail.com>
2025-09-03 21:10:34 -0500

    Added function headers to printopening() and printclosing() functions

--- print_functions.cpp
+++ print_functions.cpp
@@ -7,9 +7,27 @@
 ************************************************* */
 
 void printopening() {
+    /* *************************************************
+    * Prints the opening message to indicate the start
+    * of the testing process.
+    *
+    * @param none
+    * @return none
+    * @exception none
+    * ************************************************* */
+
     std::cout << "Beginning tests..." << std::endl;
 }
 
 void printclosing() {
+    /* *************************************************
+    * Prints the closing message to indicate the end
+    * of the testing process.
+    *
+    * @param none
+    * @return none
+    * @exception none
+    * ************************************************* */
+
     std::cout << "Testing complete." << std::endl;
 }
