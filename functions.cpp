6b1cb5e9bdcb9b0e6f2f8a9d60c733966f4e7c8d
Alfonso R <alfonsorocha101@gmail.com>
2025-09-03 23:02:10 -0500

    Added underFlowTest() implementation to functions.cpp. not completed yet

--- functions.cpp
+++ functions.cpp
@@ -5,3 +5,62 @@
 *           all violations of best practices on
 *           definition file, print_functions.cpp
 ************************************************* */
+
+#include "functions.h"
+#include "stack.h"
+
+void underFlowTests(Stack& stack) {
+    /************************************************
+    ******* UNDERFLOW TESTS *************************
+    ************************************************/
+
+    std::cout << "  underflow tests ==> ";
+
+    for (int i = 0; i < STACKSIZE*MULTIPLIER; i++) {
+
+        if (stack.isEmpty()) is_empty++;
+        else is_empty--;
+
+        if(stack.peek(&value)) peeked++;
+        else peeked--;
+
+        try{
+            value = stack.pop();
+            popped++;
+        } catch(...){
+            // should always decrement
+            popped--;
+        }
+
+        if (stack.push(i)){
+            // should always increment
+            pushed++;
+            if(stack.peek(&value)){
+                // should always increment
+                peeked++;
+            } else {
+                peeked--;
+            }
+            try{ // returns to empty
+                // should always increment
+                value = stack.pop();
+                popped++;
+            } catch(...){
+                popped--;
+            }
+        } else{
+            pushed--;
+        }
+    }
+
+    // notice these numbers are logical
+    // in underflow, we should expect these numbers
+    if(popped == 0 &&
+       peeked == 0 &&
+       pushed == round(STACKSIZE*MULTIPLIER) &&
+       is_empty == round(STACKSIZE*MULTIPLIER)) {
+        std::cout << "pass" << std::endl;
+       } else {
+           std::cout << "failed" << std::endl;
+       }
+}
